725d866624230117be89108f6da0159befcc7425
Marcelo Ceolin <fxsobr@gmail.com>
2022-11-08 21:34:18 -0300

    criado função de game over

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -3,6 +3,7 @@ defmodule ExMon do
   alias ExMon.Game.{Actions, Status}
 
   @computer_name "Robotinik"
+  @computer_moves [:move_average, :move_random, :move_heal]
 
   def create_player(name, move_average, move_random, move_heal) do
     Player.build(name, move_average, move_random, move_heal)
@@ -17,10 +18,21 @@ defmodule ExMon do
   end
 
   def make_move(move) do
+    Game.info()
+    |> Map.get(:status)
+    |> handle_status(move)
+  end
+
+  defp handle_status(:game_over, _move), do: Status.print_round_message(Game.info())
+  defp handle_status(_other, move) do
     move
     |> Actions.fetch_move()
     |> do_move()
+
+    computer_move(Game.info())
   end
+
+
 
   defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
   defp do_move({:ok, move}) do
@@ -31,4 +43,10 @@ defmodule ExMon do
     Status.print_round_message(Game.info())
   end
 
+  defp computer_move(%{turn: :computer, status: :continue}) do
+    move = {:ok, Enum.random(@computer_moves)}
+    do_move(move)
+  end
+
+  defp computer_move(_), do: :ok
 end
